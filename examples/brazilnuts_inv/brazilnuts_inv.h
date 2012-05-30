b30f8ec1992cea30bc33331a98bc29bab0284142
Matt Bierbaum <m-bierbaum@jdubs.(none)>
2012-05-30 15:51:15 -0400

    tweak examples

--- examples/brazilnuts_inv/brazilnuts_inv.h
+++ examples/brazilnuts_inv/brazilnuts_inv.h
@@ -1,14 +1,14 @@
 #define CONST_PBC               {0,0}
 #define CONST_PARTICLECOUNT     512*2
-#define CONST_KICKFORCE         1.0f
+#define CONST_KICKFORCE         10.0f
 #define CONST_FRAME_SKIP        10
 #define CONST_COLOR_FACTOR      500.0f
 
 #define INIT_RANDOM_FRACTION    4.5f
-#define FUNCTION_INIT           {INIT_RANDOM rad[0]=1.5f;}
+#define FUNCTION_INIT           {INIT_RANDOM rad[0]=2.0f; for (i=0; i<N; i++) {if (i%2==1) rad[i] = 1.1f;}}
 
-#define CONST_CUTOFF_FACTOR     5.5
-#define FORCE_MORSE_EPSILON     1830.0
+#define CONST_CUTOFF_FACTOR     5.0
+#define FORCE_MORSE_EPSILON     2000.0
 #define FORCE_MORSE_ALPHA       0.10
 #define FUNCTION_FORCE_PAIR     FORCE_MORSE
 
--- examples/sand/sand.h
+++ examples/sand/sand.h
@@ -4,10 +4,10 @@
 
 #define ARGV1           FORCE_HERTZ_EPSILON 
 #define ARGV1_TYPE      double 
-#define ARGV1_DEFAULT   100.0
+#define ARGV1_DEFAULT   200.0
 
 #define INIT_RANDOM_FRACTION    2.1
-#define FUNCTION_INIT           {INIT_RANDOM rad[0] = 5; type[0] = RED;}
+#define FUNCTION_INIT           {INIT_RANDOM rad[0] = 3; type[0] = RED;}
 
 #define FORCE_HERTZ_EPSILON     ARGV1_VAR 
 #define FUNCTION_FORCE_PAIR     FORCE_HERTZ
