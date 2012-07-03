13add183a3b90c435bf77fddc510f7f185ea3ab6
Matt Bierbaum <m-bierbaum@jdubs.(none)>
2012-07-03 16:22:31 -0400

    starting to fill out fields

--- core/fields.h
+++ core/fields.h
@@ -2,13 +2,13 @@
 #define __FIELDS_H__
 
 typedef struct {
-    int size[2];
+    int size[3];
     int numcomps;
     float *data;
 } field;
 
-field *fields_calculate_density(int N, float L, float *x);
-field *fields_calculate_velocity(int N, float L, float *x, float *v);
+field *fields_calculate_scalar(int N, float L, float *x, float *s, int S);
+field *fields_calculate_vector(int N, float L, float *x, float *v, int S);
 
 float *fields_value_at(field *fld, float *x);
 float *fields_dfdx_at(field *fld, float *x);
