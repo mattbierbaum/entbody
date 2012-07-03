8cacfea163c6491561cba88fbcd21e96ec4325c2
Matt Bierbaum <m-bierbaum@jdubs.(none)>
2012-07-03 16:23:00 -0400

    didn't mean to add that

--- core/fields.c
+++ core/fields.c
@@ -1,14 +1,14 @@
 #include "fields.h"
 
 field *fields_calculate_scalar(int N, float L, float *x, float *s, int S){
-    field *rho = (field*)malloc(sizeof(field));
+/*    field *rho = (field*)malloc(sizeof(field));
 
     rho->size[0]  = S;
     rho->size[1]  = S;
     rho->size[2]  = (DIM>2)?S:1;
     rho->numcomps = 1;
     rho->data  = (float*(malloc(sizeof(float)*
-
+*/
 }
 
 
